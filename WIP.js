d8be66a868539f153e74b54d191294a91e446371
TeaBun <stephen.alberts@gmail.com>
2022-02-08 11:08:54 -0600

    new project page

--- WIP.js
+++ WIP.js
@@ -13,6 +13,9 @@ function setup() {
   stroke(255)
   // rect(400,400,200,200)
   console.log((width-200))
+
+  
+  
 }
 
 var x = 0
@@ -26,9 +29,6 @@ var g = 127
 var b = 127
 
 setInterval(function(){
-  // fill(0,0,0,10)
-  // noFill()
-  // rect(-500,-500,1000,1000)
   r = r + floor(random(-5,6))
   g = g + floor(random(-5,6))
   b = b + floor(random(-5,6))
@@ -51,47 +51,92 @@ setInterval(function(){
   if(b > 250){
     b = 245
   }
-  // var rgb = [r,g,b]
-  // console.log(rgb)
-
 },100);
 
 function draw() {
-  // strokeWeight(0.1)
-  // noFill()
-  // circle(-100,0,r*2)
-  // circle(0,0,g*2)
-  // circle(100,0,b*2)
-
   
-  // rotate(40)
-
-
-  movex = floor(random(-42,43))
-  movey = floor(random(-42,43))
+  movex = floor(random(-21,22))
+  movey = floor(random(-21,22))
   x = x + movex
   y = y + movey
 
   var v = createVector(x, y)
   grid.push(v)
   
-  fill(r,g,b,168)
+  // fill(r,g,b,168)
   // noFill()
-  stroke(r,g,b,floor(random(10)))
+  // stroke(r,g,b,floor(random(10)))
   // noStroke()
   
   push()
-  // translate(p5.Vector.fromAngle(millis() / 1000, 200));
+  translate(p5.Vector.fromAngle(millis() / 1000, 200));
 
   translate(grid[grid.length-1].x,grid[grid.length-1].y)
 
   rotateX(frameCount * 0.01);
   rotateY(frameCount * 0.01);
+  noStroke()
+  fill(r,g,b)
   box(r/4,g/4,b/4)
+  
   pop()
 
-  strokeWeight(1)
+  // strokeWeight(1)
 
+  // background(0)
+  // fill(r,g,b,222)
+  // stroke(r,g,b)
+  // box(r,g,b)
+
+  push()
+  fill(r,g,b,86)
+  noStroke()
+  strokeWeight(8)
+  translate(grid[grid.length-1].x,grid[grid.length-1].y)
+  translate(r-(r*0.5),g-(g*0.5),b-(b*0.5))
+  sphere(42)
+  pop()
+
+  // push()
+  // stroke("red")
+  // strokeWeight(8)
+  // line(r-(r*0.5),-r+(r*0.5),g-(g*0.5),g-(g*0.5),b-(b*0.5),b-(b*0.5))
+  // pop()
+
+  // push()
+  // stroke(255)
+  // strokeWeight(8)
+  // line(r-(r*0.5),r-(r*0.5),g-(g*0.5),-g+(g*0.5),b-(b*0.5),b-(b*0.5))
+  // pop()
+
+  // push()
+  // stroke(255)
+  // strokeWeight(8)
+  
+  // line(r-(r*0.5),r-(r*0.5),g-(g*0.5),g-(g*0.5),b-(b*0.5),-b+(b*0.5))
+  
+  // pop()
+
+
+  push()
+  translate(grid[grid.length-1].x,grid[grid.length-1].y)
+
+  rotateX(frameCount * 0.008)
+  rotateY(frameCount * 0.008)
+  stroke(r,g,b,floor(random(1)))
+  // noStroke()
+  fill(r,g,b,168)
+  strokeWeight(floor(random(0,4)))
+
+  beginShape()
+  vertex(-r+(r*0.5),g-(g*0.5),b-(b*0.5))
+  vertex(r-(r*0.5),-g+(g*0.5),b-(b*0.5))
+  vertex(r-(r*0.5),g-(g*0.5),-b+(b*0.5))
+  vertex(-r+(r*0.5),g-(g*0.5),b-(b*0.5))
+  endShape()
+  
+  pop()
+  
 
   if(x > ((width/2)-10)){
     x = x - 10
